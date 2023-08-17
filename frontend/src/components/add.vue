c6ade4882b789e63d204d9d23f1e218bcb108711
Alper Çıtak <citak.alper@gmail.com>
2023-08-17 16:57:06 +0200

    refactoring

--- frontend/src/components/add.vue
+++ frontend/src/components/add.vue
@@ -1,20 +1,21 @@
 <template>
   <div class="flex flex-col">
     <div class="flex">
-      <input type="number" v-model="numberA" />
-      <input type="number" v-model="numberB" />
+      <input type="number" v-model="data.a" />
+      <input type="number" v-model="data.b" />
     </div>
 
-    <div>Local result: {{ add(numberA, numberB) }}</div>
+    <div>Local result: {{ localResult }}</div>
   </div>
 </template>
 
 <script setup>
-import { ref } from 'vue';
+import { ref, computed } from 'vue';
 import { add } from 'shared';
 
-const numberA = ref(1);
-const numberB = ref(2);
+const data = ref({ a: 1, b: 2 });
+
+const localResult = computed(() => add(data.value.a, data.value.b));
 </script>
 
 <style>
