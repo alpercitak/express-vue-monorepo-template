4377ecd301ae3afa3e08c075b5cd31beb4888496
Alper Çıtak <citak.alper@gmail.com>
2023-08-17 16:32:36 +0200

    added shared function

--- frontend/src/components/hello-world.vue
+++ frontend/src/components/hello-world.vue
@@ -1,11 +1,12 @@
 <script setup>
+import { add } from 'shared';
 import { ref } from 'vue';
 
-defineProps({
-  msg: String,
-});
+const numberA = ref(1);
+const numberB = ref(2);
 </script>
 
 <template>
   <div>Hello World</div>
+  <div>Local add: {{ add(numberA, numberB) }}</div>
 </template>
