4efa5ab7eb7f642a82391a79d6c283e4b4e42470
Alper Çıtak <citak.alper@gmail.com>
2023-08-17 16:48:54 +0200

    backend endpoint for add function

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,10 +1,15 @@
 import express from 'express';
-import { serverPort } from 'shared';
+import { serverPort, add } from 'shared';
 
 const app = express();
 
 app.get('/', (req, res) => {
   return res.send({ message: 'Hello World' });
+});
+
+app.get('/add', (req, res) => {
+  const result = add(parseInt(req.query.a), parseInt(req.query.b));
+  return res.send({ result });
 });
 
 if (import.meta.env.PROD) {
